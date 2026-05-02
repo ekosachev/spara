f0dcb05266c74d2f0b4cc3aab523a8b45330328a
Egor Kosachev <egorkosachev@vk.com>
2026-05-02 23:08:35 +0300

    remove DTO func

--- back/internal/models/user.go
+++ back/internal/models/user.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"github.com/ekosachev/spara/internal/dto"
 	"gorm.io/gorm"
 )
 
@@ -10,17 +9,4 @@ type User struct {
 	Email        string
 	Username     string
 	PasswordHash string
-}
-
-func (u *User) DTO() dto.User {
-	return dto.User{
-		Model: dto.Model{
-			ID:        u.ID,
-			CreatedAt: u.CreatedAt,
-			UpdatedAt: u.UpdatedAt,
-		},
-		Email:        u.Email,
-		Username:     u.Username,
-		PasswordHash: u.PasswordHash,
-	}
 }
