c12a2c0f0b48a916af38496ffacc84ce84d5b4c2
Egor Kosachev <egorkosachev@vk.com>
2026-05-02 23:46:01 +0300

    add base dto for api response

--- back/internal/dto/base_dto.go
+++ back/internal/dto/base_dto.go
@@ -7,3 +7,9 @@ type Model struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+
+type ApiResponse struct {
+	Success bool   `json:"success"`
+	Error   string `json:"error" binding:"omitempty"`
+	Data    any    `json:"data" binding:"omitempty"`
+}
