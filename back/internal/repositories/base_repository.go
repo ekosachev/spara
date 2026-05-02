756581f42f3bfe6a013c0ac511a50b331abb2715
Egor Kosachev <egorkosachev@vk.com>
2026-05-02 23:26:26 +0300

    comment unused methods

--- back/internal/repositories/base_repository.go
+++ back/internal/repositories/base_repository.go
@@ -4,8 +4,8 @@ import "context"
 
 type BaseRepository[T any] interface {
 	Create(ctx context.Context, dto *T) error
-	GetAll(ctx context.Context) ([]T, error)
-	GetByID(ctx context.Context, id uint) (*T, error)
-	Update(ctx context.Context, id uint, dto T) (int, error)
-	Delete(ctx context.Context, id uint) (int, error)
+	// GetAll(ctx context.Context) ([]T, error)
+	// GetByID(ctx context.Context, id uint) (*T, error)
+	// Update(ctx context.Context, id uint, dto T) (int, error)
+	// Delete(ctx context.Context, id uint) (int, error)
 }
