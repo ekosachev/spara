b414eec07276ae3b6b68c628493c6212ea98b3e7
Egor Kosachev <egorkosachev@vk.com>
2026-05-02 23:31:13 +0300

    add new user repo method

--- back/internal/repositories/user_repository.go
+++ back/internal/repositories/user_repository.go
@@ -12,6 +12,10 @@ type UserRepository struct {
 	db *gorm.DB
 }
 
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return UserRepository{db}
+}
+
 func (r *UserRepository) Create(ctx context.Context, dto dto.User) error {
 	return r.db.Model(models.User{}).WithContext(ctx).Transaction(func(tx *gorm.DB) error {
 		return tx.Create(models.User{
