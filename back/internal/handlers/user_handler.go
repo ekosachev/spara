71823c88de657520507c12ad522e7dcd40625250
Egor Kosachev <egorkosachev@vk.com>
2026-05-03 00:02:51 +0300

    add create handler for user

--- back/internal/handlers/user_handler.go
+++ back/internal/handlers/user_handler.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"log/slog"
+	"net/http"
 
+	"github.com/ekosachev/spara/internal/dto"
 	"github.com/ekosachev/spara/internal/services"
 	"github.com/gin-gonic/gin"
 )
@@ -20,8 +22,30 @@ func (h *UserHandler) RegisterRoutes(group gin.RouterGroup, prefix string) {
 	routerGroup := group.Group(prefix)
 
 	{
-		// register routes here
+		routerGroup.POST("/", h.create)
 	}
 }
 
-func (h *UserHandler) Create(c *gin.Context)
+func (h *UserHandler) create(c *gin.Context) {
+	var req dto.CreateUserRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		sendError(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := h.service.Create(c, dto.User{
+		Email:        req.Email,
+		Username:     req.Username,
+		PasswordHash: req.Password, // implement hashing later
+	}); err != nil {
+		h.logger.Error("Failed to create user", slog.String("error", err.Error()))
+		sendError(c, http.StatusInternalServerError, "Internal server error")
+		return
+	}
+
+	c.JSON(http.StatusOK, dto.ApiResponse{
+		Success: true,
+		Data:    "User created",
+	})
+}
