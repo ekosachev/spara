69e0799f2f942fa489bb53f437b8d82db66a4bee
Egor Kosachev <egorkosachev@vk.com>
2026-05-02 23:26:03 +0300

    add user repo with Create method

---
+++ back/internal/repositories/user_repository.go
@@ -0,0 +1,23 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/ekosachev/spara/internal/dto"
+	"github.com/ekosachev/spara/internal/models"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func (r *UserRepository) Create(ctx context.Context, dto dto.User) error {
+	return r.db.Model(models.User{}).WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		return tx.Create(models.User{
+			Email:        dto.Email,
+			Username:     dto.Username,
+			PasswordHash: dto.PasswordHash,
+		}).Error
+	})
+}
