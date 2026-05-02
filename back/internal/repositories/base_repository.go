2764cb1fbdfc29c8ad698c455bb8d69d99c007d6
Egor Kosachev <egorkosachev@vk.com>
2026-05-02 23:13:37 +0300

    add base repo interface

---
+++ back/internal/repositories/base_repository.go
@@ -0,0 +1,11 @@
+package repositories
+
+import "context"
+
+type BaseRepository[T any] interface {
+	Create(ctx context.Context, dto *T) error
+	GetAll(ctx context.Context) ([]T, error)
+	GetByID(ctx context.Context, id uint) (*T, error)
+	Update(ctx context.Context, id uint, dto T) (int, error)
+	Delete(ctx context.Context, id uint) (int, error)
+}
