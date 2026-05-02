1fca3c9e41fb67c129f487f119040c955da71b5a
Egor Kosachev <egorkosachev@vk.com>
2026-05-02 23:44:00 +0300

    add dtos for user creation

--- back/internal/dto/user.go
+++ back/internal/dto/user.go
@@ -6,3 +6,15 @@ type User struct {
 	Username     string
 	PasswordHash string
 }
+
+type CreateUserRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UserResponse struct {
+	ID       uint   `json:"id"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
+}
