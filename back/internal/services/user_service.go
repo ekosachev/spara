6cfe72afa7290e65ad98ac5080caeb554ce642d0
Egor Kosachev <egorkosachev@vk.com>
2026-05-03 16:57:32 +0300

    add pasword hashing to user service

--- back/internal/services/user_service.go
+++ back/internal/services/user_service.go
@@ -1,8 +1,11 @@
 package services
 
 import (
+	"context"
+
 	"github.com/ekosachev/spara/internal/dto"
 	"github.com/ekosachev/spara/internal/repositories"
+	"github.com/ekosachev/spara/internal/utils"
 )
 
 type UserService struct {
@@ -13,4 +16,19 @@ func NewUserService(repo repositories.BaseRepository[dto.User]) UserService {
 	return UserService{
 		BaseServise: BaseServise[dto.User]{repo: repo},
 	}
+}
+
+func (s *UserService) Create(ctx context.Context, user dto.User) (*dto.User, error) {
+	hashedPassword, err := utils.HashPassword(user.PasswordHash)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return s.repo.Create(ctx, dto.User{
+		Model:        user.Model,
+		Email:        user.Email,
+		PasswordHash: hashedPassword,
+		Username:     user.Username,
+	})
 }
