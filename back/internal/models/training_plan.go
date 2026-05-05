a89544d147741edc27fed12b94339005fcea84b9
Egor Kosachev <egorkosachev@vk.com>
2026-05-05 22:40:15 +0300

    add many2many between excercise and training plan

--- back/internal/models/training_plan.go
+++ back/internal/models/training_plan.go
@@ -4,6 +4,13 @@ import "gorm.io/gorm"
 
 type TrainingPlan struct {
 	gorm.Model
-	Name   string
-	UserID uint
+	Name       string
+	UserID     uint
+	Excercices []*Excercise `gorm:"many2many:training_plan_excercises"`
+}
+
+type TrainingPlanExcercises struct {
+	TrainingPlanID int `gorm:"primaryKey"`
+	ExcerciseID    int `gorm:"primaryKey"`
+	Order          int
 }
