7b8598d1fd825f5d3197da1197fc5fb369f5ce44
Egor Kosachev <egorkosachev@vk.com>
2026-05-05 12:52:27 +0300

    remove permissions from GenerateToken

--- back/internal/utils/jwt.go
+++ back/internal/utils/jwt.go
@@ -7,12 +7,11 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
-func GenerateToken(userID uint, permissions []string) (string, error) {
+func GenerateToken(userID uint) (string, error) {
 	cfg := config.GetConfig()
 	claims := jwt.MapClaims{
-		"sub":         userID,
-		"exp":         time.Now().Add(time.Duration(cfg.JWTExpirationSeconds) * time.Second).Unix(),
-		"permissions": permissions,
+		"sub": userID,
+		"exp": time.Now().Add(time.Duration(cfg.JWTExpirationSeconds) * time.Second).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
