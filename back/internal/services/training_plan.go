ef0d2cf642b957ace437bf5ae8e5e89aef385089
Egor Kosachev <egorkosachev@vk.com>
2026-05-05 19:36:05 +0300

    add user check in create training plan

--- back/internal/services/training_plan.go
+++ back/internal/services/training_plan.go
@@ -9,17 +9,22 @@ import (
 
 type TrainingPlanServcie struct {
 	BaseServise[dto.TrainingPlan]
-	repo repositories.TrainingPlanRepository
+	repo     repositories.TrainingPlanRepository
+	userRepo repositories.UserRepository
 }
 
-func NewTrainingPlanService(repo repositories.TrainingPlanRepository) TrainingPlanServcie {
+func NewTrainingPlanService(repo repositories.TrainingPlanRepository, userRepo repositories.UserRepository) TrainingPlanServcie {
 	return TrainingPlanServcie{
 		BaseServise: BaseServise[dto.TrainingPlan]{repo: repo},
 		repo:        repo,
+		userRepo:    userRepo,
 	}
 }
 
 func (s *TrainingPlanServcie) Create(ctx context.Context, trainingPlan dto.TrainingPlan) (*dto.TrainingPlan, error) {
+	if _, err := s.userRepo.GetByID(ctx, trainingPlan.UserID); err != nil {
+		return nil, err
+	}
 
 	return s.repo.Create(ctx, trainingPlan)
 }
