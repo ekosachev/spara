10fda1e41b38a28a34da7c10fe2622da592df0c9
Egor Kosachev <egorkosachev@vk.com>
2026-05-05 20:14:17 +0300

    fix auth middleware

--- back/internal/middleware/auth_middleware.go
+++ back/internal/middleware/auth_middleware.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/ekosachev/spara/internal/config"
 	"github.com/ekosachev/spara/internal/dto"
 	"github.com/gin-gonic/gin"
@@ -10,6 +13,7 @@ import (
 func AuthMiddleware() gin.HandlerFunc {
 	var cfg = config.GetConfig()
 	var secretKey = cfg.JWTSecret
+
 	return func(ctx *gin.Context) {
 		authHeader := ctx.GetHeader("Authorization")
 		if authHeader == "" {
@@ -17,11 +21,24 @@ func AuthMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		tokenString := authHeader[7:] // remove "Bearer: " prefix
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			ctx.AbortWithStatusJSON(401, dto.ApiResponse{Success: false, Error: "Invalid auth header"})
+			return
+		}
 
-		token, _ := jwt.Parse(tokenString, func(t *jwt.Token) (any, error) {
-			return secretKey, nil
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(t *jwt.Token) (any, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method")
+			}
+			return []byte(secretKey), nil
 		})
+
+		if err != nil {
+			ctx.AbortWithStatusJSON(401, dto.ApiResponse{Success: false, Error: err.Error()})
+			return
+		}
 
 		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 			ctx.Set("userID", claims["sub"])
