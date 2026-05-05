db7e358161daa2d425f31e5767a2be996d5076ee
Egor Kosachev <egorkosachev@vk.com>
2026-05-05 22:54:03 +0300

    update excercise dtos for training plans

--- back/internal/dto/training_plan.go
+++ back/internal/dto/training_plan.go
@@ -8,13 +8,18 @@ type TrainingPlan struct {
 }
 
 type TrainingPlanExcercise struct {
+	Order     int
+	Excercise Excercise
+}
+
+type TrainingPlanExcerciseRequest struct {
 	Order       int `json:"order" binding:"required"`
 	ExcerciseID int `json:"excercise_id" binding:"required"`
 }
 
 type CreateTrainingPlanRequest struct {
-	Name       string                  `json:"name" binding:"required"`
-	Excercises []TrainingPlanExcercise `json:"excercises" binding:"omitempty"`
+	Name       string                         `json:"name" binding:"required"`
+	Excercises []TrainingPlanExcerciseRequest `json:"excercises" binding:"omitempty"`
 }
 
 type TrainingPlanResponse struct {
