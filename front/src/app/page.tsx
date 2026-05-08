f5877371c71c66042af34866bef96abc21ef5bfe
Egor Kosachev <egorkosachev@vk.com>
2026-05-09 00:30:35 +0300

    add excercise list to page

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,3 +1,22 @@
+'use client';
+
+import { useExcercises } from "@/hooks/useExcercises";
 
 export default function Home() {
+    const { data: excercises, isLoading, error } = useExcercises();
+
+    if (isLoading) return (<div>Грузим железки...</div>);
+
+    if (error) return (<div>Бэк упал или CORS отвалился</div>);
+
+    return (
+        <main>
+            <h1>Упражнения</h1>
+            {excercises?.map((ex) => (
+                <div key={ex.id}>
+                    <h2>{ex.name}</h2>
+                </div>
+            ))}
+        </main>
+    )
 }
