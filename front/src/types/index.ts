4ddd9b1bd9c7208559f035123f01041ee869350f
Egor Kosachev <egorkosachev@vk.com>
2026-05-08 22:49:00 +0300

    add interfaces for user and excercise

---
+++ front/src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Excercise {
+    id: number;
+    name: string;
+    description: string;
+}
+
+export interface User {
+    id: number;
+    email: string;
+    username: string;
+}
