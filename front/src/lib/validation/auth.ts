6a02ec281ab4cdb26abdb3a828b8378010086c70
Egor Kosachev <egorkosachev@vk.com>
2026-05-12 17:57:04 +0300

    change messages to english

--- front/src/lib/validation/auth.ts
+++ front/src/lib/validation/auth.ts
@@ -1,14 +1,14 @@
 import * as z from "zod"
 
 export const loginSchema = z.object({
-    email: z.string().email({ message: "Некорректный формат email-адреса" }),
-    password: z.string().min(6, { message: "Пароль должен быть не менее чем из 6 символов" })
+    email: z.string().email({ message: "Incorrect email" }),
+    password: z.string().min(6, { message: "Password too short. Use at least 6 characters" })
 })
 
 export const registerSchema = z.object({
-    email: z.string().email({ message: "Некорректный формат email-адреса" }),
-    username: z.string().min(6, { message: "Имя пользователя должно быть длиннее 5 символов" }),
-    password: z.string().min(6, { message: "Пароль должен быть не менее чем из 6 символов" })
+    email: z.string().email({ message: "Incorrect email" }),
+    username: z.string().min(6, { message: "Username too short. Use at least 6 letters" }),
+    password: z.string().min(6, { message: "Password too short. Use at least 6 characters" })
 })
 
 export type LoginInput = z.infer<typeof loginSchema>
